f76ad85993d494fa5469fdd9e0c21bfd13ecc5f3
dnsrthn <ethanjupi@gmail.com>
2024-09-04 08:23:22 -0600

    build(proyecto): se crea el proyecto de spring

---
+++ src/main/java/com/limsolutions/hotelerialim/HotelsLimApplication.java
@@ -0,0 +1,13 @@
+package com.limsolutions.hotelerialim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsLimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsLimApplication.class, args);
+	}
+
+}
