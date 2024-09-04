7f9c1382a282587a8d8b2d86219f19e1cb251958
Dgarcia-09 <dgarcia-2023532@kinal.edu.gt>
2024-09-04 11:25:29 -0600

    add(proyecto):Agregamos la clase pais

--- src/main/java/com/limsolutions/hotelerialim/models/pais.java
+++ src/main/java/com/limsolutions/hotelerialim/models/pais.java
@@ -3,18 +3,23 @@ package  com.limsolutions.hotelerialim.models;
 import java.io.Serializable;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@
 @Data
 @Table(name = "pais")
 public class pais implements Serializable{
     @Id
     @Column(name = "id")
-        private Integer id;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
 
         
    
