1226a8db529b5d423718ad606f5af9b4675c7f58
Dgarcia-09 <anatareno-2019067@kinal.edu.gt>
2024-09-04 11:35:33 -0600

    feat(models):Cambiamos caracteristicas en la clase de pais

--- src/main/java/com/limsolutions/hotelerialim/models/pais.java
+++ src/main/java/com/limsolutions/hotelerialim/models/pais.java
@@ -3,6 +3,7 @@ package  com.limsolutions.hotelerialim.models;
 import java.io.Serializable;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,7 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@
+@Entity
 @Data
 @Table(name = "pais")
 public class pais implements Serializable{
