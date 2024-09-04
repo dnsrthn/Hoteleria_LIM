78517a315f17b5dc4540057216d59b6bb441fd1f
dnsrthn <ethanjupi@gmail.com>
2024-09-04 09:09:42 -0600

    add(proyecto): se agregan los archivos a la rama

---
+++ src/test/java/com/limsolutions/hotelerialim/HotelsLimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limsolutions.hotelerialim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsLimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
