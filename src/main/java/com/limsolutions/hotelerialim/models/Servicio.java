88bfe1382586cb368d564fc253a84227f852deb1
ethan <ethanjupi@gmail.com>
2024-09-06 19:05:16 -0600

    add (models) resto de modelos para siguiete face de trabajo

---
+++ src/main/java/com/limsolutions/hotelerialim/models/Servicio.java
@@ -0,0 +1,31 @@
+package com.limsolutions.hotelerialim.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "servicio")
+public class Servicio implements Serializable{
+    @Id
+    @Column(name = "id_servcio", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_servicio;
+
+    @OneToOne
+    @JoinColumn(name = "id_hotel", nullable = false)
+    private Hotel id_hotel;
+
+    private String nombre;
+
+
+}
