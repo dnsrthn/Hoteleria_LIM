2d66b51afbf723ea207e24cbfbca64c853ae6e6a
AnthonyEscobarLaboral <aescobar-2020229@kinal.edu.gt>
2024-09-07 14:38:21 -0600

    feat(config): agregue clase para tecnologia de cloudinary administracion de credenciales

---
+++ src/main/java/com/limsolutions/hotelerialim/config/CloudinaryConfig.java
@@ -0,0 +1,20 @@
+package com.limsolutions.hotelerialim.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Bean
+    Cloudinary cloudinary(){
+        return new Cloudinary(ObjectUtils.asMap(
+        "cloud_name","hoteleriaLim",
+        "api_key","575864119997421",
+        "api_secret","WXXZRkhZPqtyw3tuP_GjzbqElfE"
+        ));
+    }
+}
