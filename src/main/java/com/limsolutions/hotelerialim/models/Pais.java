b899eaeede046912b10a831bcfcda97ef953f14a
ethan <ethanjupi@gmail.com>
2024-09-06 19:12:49 -0600

    add (models) resto de modelos para siguiete face de trabajo

---
+++ src/main/java/com/limsolutions/hotelerialim/models/Pais.java
@@ -0,0 +1,29 @@
+package  com.limsolutions.hotelerialim.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "Pais")
+public class Pais implements Serializable{
+    @Id
+    @Column(name = "id_pais")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id_pais;
+
+    @NotBlank(message = "Necesita llenar el campo de nombre")
+    private String nombre;
+
+        
+   
+}
