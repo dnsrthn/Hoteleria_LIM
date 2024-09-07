09fcd66dae8a0b0c7110ef26aed6fa2105dbca88
Ethan <ethanjupi@gmail.com>
2024-09-06 20:47:26 -0600

    feat (service) clase service de hotel

---
+++ src/main/java/com/limsolutions/hotelerialim/service/Hotel.java
@@ -0,0 +1,38 @@
+package com.limsolutions.hotelerialim.service;
+
+import com.limsolutions.hotelerialim.models.Hotel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelService implements IHotel {
+    private List<Hotel> hoteles = new ArrayList<>();
+
+    @Override
+    public void registrarHotel(Hotel hotel) {
+        hoteles.add(hotel);
+    }
+
+    @Override
+    public void actualizarHotel(Hotel hotel) {
+        for (int i = 0; i < hoteles.size(); i++) {
+            if (hoteles.get(i).getId() == hotel.getId()) {
+                hoteles.set(i, hotel);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public List<Hotel> obtenerHoteles() {
+        return hoteles;
+    }
+
+    @Override
+    public Hotel obtenerHotelId(int idHotel) {
+        return hoteles.stream()
+                .filter(h -> h.getId() == idHotel)
+                .findFirst() //no es necesario importar las dependencias de stream para usar FindFirst
+                .orElse(null);
+    }
+}
