79fa0b3072c1aa7b03ae1792a5b860a229a143ae
Javier <javiherdeza@gmail.com>
2024-09-06 23:45:50 -0600

    efactor(eliminarHabitacion): actualizar la lógica para manejar habitación no encontrada

--- src/main/java/com/limsolutions/hotelerialim/controller/HabitacionController.java
+++ src/main/java/com/limsolutions/hotelerialim/controller/HabitacionController.java
@@ -6,10 +6,10 @@ import com.limsolutions.hotelerialim.service.IHabitacionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,16 +35,17 @@ public class HabitacionController {
     }
 
     @GetMapping("/habitaciones/{id_habitacion}")
-    public ResponseEntity <Habitacion> buscarHabitacion(@PathVariable Long id_habitacion){
+    public ResponseEntity<Habitacion> buscarHabitacion(@PathVariable Long id_habitacion) {
         Habitacion habitacion = IHabitacionService.buscarHabitacion(id_habitacion);
 
         return ResponseEntity.ok(habitacion);
     }
 
     @PutMapping("/habitaciones/{id_habitacion}")
-    public ResponseEntity <Habitacion> editarHabitacion(@PathVariable Long id_habitacion, @RequestBody Habitacion habitacionRecibido){
+    public ResponseEntity<Habitacion> editarHabitacion(@PathVariable Long id_habitacion,
+            @RequestBody Habitacion habitacionRecibido) {
         Habitacion habitacion = IHabitacionService.buscarHabitacion(id_habitacion);
-        
+
         habitacion.setTipo(habitacionRecibido.getTipo());
         habitacion.setPrecio(habitacionRecibido.getPrecio());
         habitacion.setNumero(habitacionRecibido.getNumero());
@@ -56,14 +57,18 @@ public class HabitacionController {
     }
 
     @DeleteMapping("/habitaciones/{id_habitacion}")
-    public ResponseEntity<Map<String, Boolean> eliminarHabitacion(@PathVariable Long id_habitacion){
+    public ResponseEntity<Map<String, Boolean>> eliminarHabitacion(@PathVariable Long id_habitacion) {
         Habitacion habitacion = IHabitacionService.buscarHabitacion(id_habitacion);
+
+        if (habitacion == null) {
+            // Habitación no encontrada
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Eliminado", false));
+        }
 
         IHabitacionService.eliminarHabitacion(habitacion);
 
-        Map<String, Boolean> respuesta = new HashMap<>();
-        respuesta.put("Eliminado", true);
-        return ResponseEntity.ok(respuesta);
-
+        // Habitación eliminada con éxito
+        return ResponseEntity.ok(Map.of("Eliminado", true));
     }
+
 }
