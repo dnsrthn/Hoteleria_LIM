12b3a88dc8db7c6d71fbcf267dcb6c3dd3655c18
M0B7 <dgarcia-2023532@kinal.edu.gt>
2024-09-10 08:55:19 -0600

    feat(service-IServicioService): Se creo la interfaz de la entidad servicio

---
+++ src/main/java/com/limsolutions/hotelerialim/service/IServicioService.java
@@ -0,0 +1,19 @@
+package com.limsolutions.hotelerialim.service;
+
+import java.util.List;
+
+import com.limsolutions.hotelerialim.models.Servicio;
+
+
+
+public interface IServicioService {
+
+    public List<Servicio> listarServicios();
+
+    public Servicio buscarServicio(Long id_servicio);
+
+    public Servicio guardarServicio (Servicio servicio);
+
+    public void eliminarServicio (Servicio servicio);
+
+}
