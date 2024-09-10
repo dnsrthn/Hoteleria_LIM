8448ad39df8ef98bad24587bfb09e46f6465bccc
M0B7 <dgarcia-2023532@kinal.edu.gt>
2024-09-10 09:24:13 -0600

    feat(Service-ServicoService): Se creo la implementacion de la interfaz de ServcioService

--- src/main/java/com/limsolutions/hotelerialim/service/ServicioService.java
+++ src/main/java/com/limsolutions/hotelerialim/service/ServicioService.java
@@ -1,8 +1,42 @@
 package com.limsolutions.hotelerialim.service;
 
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.limsolutions.hotelerialim.models.Servicio;
+import com.limsolutions.hotelerialim.repository.ServicioRepository;
 
 @Service
 public class ServicioService implements IServicioService {
+
+    @Autowired
+    private ServicioRepository servicioRepository;
+
+    @Override
+    public List<Servicio> listarServicios(){
+        return servicioRepository.findAll();
+    }
+
+    @Override
+    public Servicio buscarServicio (Long id_servicio){
+        Servicio servicio = servicioRepository.findById(id_servicio).orElse(null);
+        return servicio;
+    }
+
+    @Override
+    public Servicio guardarServicio (Servicio servicio){
+        return servicioRepository.save(servicio);
+    }
+
+    @Override 
+    public void eliminarServicio (Servicio servicio){
+        servicioRepository.delete(servicio);
+    }
+
+
+
 
 }
