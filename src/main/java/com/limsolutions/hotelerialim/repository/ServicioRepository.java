d4876939525de19f899d46a07add0b0d70e59bcc
M0B7 <dgarcia-2023532@kinal.edu.gt>
2024-09-10 09:00:32 -0600

    feat(repository-ServicioRepository): se creo el repositorio para la clase servicio

---
+++ src/main/java/com/limsolutions/hotelerialim/repository/ServicioRepository.java
@@ -0,0 +1,9 @@
+package com.limsolutions.hotelerialim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.limsolutions.hotelerialim.models.Servicio;
+
+public interface ServicioRepository extends JpaRepository<Servicio, Long>{
+
+}
---
+++ src/main/java/com/limsolutions/hotelerialim/service/ServicioService.java
@@ -0,0 +1,8 @@
+package com.limsolutions.hotelerialim.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioService implements IServicioService {
+
+}
