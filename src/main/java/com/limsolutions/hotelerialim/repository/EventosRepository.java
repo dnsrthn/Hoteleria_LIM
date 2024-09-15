a18a718252b9b17d6d1e89f21dc11415a7d97359
AnthonyEscobarLaboral <aescobar-2020229@kinal.edu.gt>
2024-09-14 23:28:30 -0600

    add(repository): agregamos repositorios de entidades eventos,salones y servicios

---
+++ src/main/java/com/limsolutions/hotelerialim/repository/EventosRepository.java
@@ -0,0 +1,11 @@
+package com.limsolutions.hotelerialim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.limsolutions.hotelerialim.models.Eventos;
+
+public interface EventosRepository extends JpaRepository <Eventos,Long>{
+
+
+
+}
